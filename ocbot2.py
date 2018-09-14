fc6264c78481e324b317277f70ac53397486c766
Zoni Nation <zonination@gmail.com>
2018-09-13 22:32:29 -0400

    Add files via upload

--- ocbot2.py
+++ ocbot2.py
@@ -14,7 +14,8 @@ import haiku  # Haiku module (file too long to be included in main)
 r=rlogin.oc()
 
 # Designate initial conditions
-sub = 'dataisbeautiful'
+sub     = 'dataisbeautiful'
+version = 'OC-Bot&nbsp;v2.03'
 
 # -------------------------
 # Primary Objective
@@ -48,10 +49,10 @@ def sticky(submission):
     print('  Citation ID: {0}'.format(earliest.id))
     
     # Construct the reply sticky
-    reply = 'Thank you for your Original Content, /u/{0}!  \n**Here is some important information about this post:**\n\n* [Author\'s citations](https://www.reddit.com{1}) for this thread\n* [All OC posts by this author](https://www.reddit.com/r/dataisbeautiful/search?q=author%3A\"{0}\"+title%3AOC&sort=new&include_over_18=on&restrict_sr=on)\n\nI hope this sticky assists you in having an informed discussion in this thread, or inspires you to [remix](https://www.reddit.com/r/dataisbeautiful/wiki/index#wiki_remixing) this data. For more information, please [read this Wiki page](https://www.reddit.com/r/dataisbeautiful/wiki/flair#wiki_oc_flair).\n\n---\n\n^^OC-Bot&nbsp;v2.02 ^^| ^^[Fork&nbsp;with&nbsp;my&nbsp;code](https://github.com/zonination/oc-bot) ^^| ^^[Message&nbsp;the&nbsp;Mods](https://www.reddit.com/message/compose?to=%2Fr%2Fdataisbeautiful&subject=Assistance%20with%20the%20Bot)'.format(submission.author.name, earliest.permalink)
+    reply = 'Thank you for your Original Content, /u/{0}!  \n**Here is some important information about this post:**\n\n* [Author\'s citations](https://www.reddit.com{1}) for this thread\n* [All OC posts by this author](https://www.reddit.com/r/dataisbeautiful/search?q=author%3A\"{0}\"+title%3AOC&sort=new&include_over_18=on&restrict_sr=on)\n\nI hope this sticky assists you in having an informed discussion in this thread, or inspires you to [remix](https://www.reddit.com/r/dataisbeautiful/wiki/index#wiki_remixing) this data. For more information, please [read this Wiki page](https://www.reddit.com/r/dataisbeautiful/wiki/flair#wiki_oc_flair).\n\n---\n\n^^{2} ^^| ^^[Fork&nbsp;with&nbsp;my&nbsp;code](https://github.com/zonination/oc-bot) ^^| ^^[Message&nbsp;the&nbsp;Mods](https://www.reddit.com/message/compose?to=%2Fr%2Fdataisbeautiful&subject=Assistance%20with%20the%20Bot)'.format(submission.author.name, earliest.permalink, version)
     submission.reply(reply).mod.distinguish(sticky=True)
     
-    # Grab latest sticky ID
+    # Grab sticky ID that OC bot just made
     for comment in r.redditor('OC-bot').comments.new(limit=1):
         last = comment.id
     print('  Sticky ID: {0}\n'.format(last))
@@ -73,7 +74,6 @@ def flair(author):
     # yet, at least give them 1 until the next reflair cycle.
     if n==0:
         n=1
-    r.subreddit(sub).flair.set(author, 'OC: {0}'.format(n), 'ocmaker')
     return n
 
 
@@ -86,7 +86,7 @@ def chkinbox():
         if item in r.inbox.comment_replies(limit=100):
             print('Comment reply from /u/{0}'.format(item.author))
             poetry = haiku.h()
-            item.reply('{0}\n\n^^OC-Bot&nbsp;v2.02 ^^| ^^[Suggest&nbsp;a&nbsp;haiku](https://www.reddit.com/message/compose?to=%2Fr%2Fdataisbeautiful&subject=Suggestion%20for%20a%20Haiku&message=For%20this%20OC%20bot,%20%20%0AI%27d%20like%20to%20submit%20a%20poem%20%20%0Aof%20my%20own%20writing:%0a%0a)'.format(poetry))
+            item.reply('{0}\n\n^^{1} ^^| ^^[Suggest&nbsp;a&nbsp;haiku](https://www.reddit.com/message/compose?to=%2Fr%2Fdataisbeautiful&subject=Suggestion%20for%20a%20Haiku&message=For%20this%20OC%20bot,%20%20%0AI%27d%20like%20to%20submit%20a%20poem%20%20%0Aof%20my%20own%20writing:%0a%0a)'.format(poetry, version))
             print('{0}\n'.format(poetry))
             item.mark_read()
         
@@ -96,7 +96,7 @@ def chkinbox():
             item.reply('Hi. I would like to reply to your message, but I am just a bot.\n\nPlease [contact the mods](https://www.reddit.com/message/compose?to=%2Fr%2Fdataisbeautiful&subject=Assistance%20with%20the%20bot) to get a human from /r/dataisbeautiful if your message is urgent.')
             print('  Sent instructions to contact mods')
             # Let the mods know of the PM from the user
-            r.redditor('/r/dataisbeautiful').message('Message from /u/{0}'.format(item.author), 'I have a message in my inbox from /u/{0}. In case this is important, [you may wish to contact them](https://www.reddit.com/message/compose?from_sr=dataisbeautiful&to={0}&subject={1}). (Be sure to select /r/dataisbeautiful from the drop-down menu). Below is what I received from them in full:\n\n---\n\n**{1}:**  \n{2}'.format(item.author, item.subject, item.body))
+            r.redditor('/r/dataisbeautiful').message('Message from /u/{0}'.format(item.author), 'I have a message in my inbox from /u/{0}. In case this is important, [you may wish to contact them](https://www.reddit.com/message/compose?from_sr=dataisbeautiful&to={0}&subject={1}). (Be sure to select /r/dataisbeautiful from the drop-down menu). Below is what I received from them in full:\n\n---\n\n**{1}**\n\n{2}'.format(item.author, item.subject, item.body))
             print('  Forwarded message to /r/dataisbeautiful\n')
             item.mark_read()
     return False
@@ -133,6 +133,7 @@ while True:
                     if submission.author_flair_css_class not in ['w', 'practitioner', 'AMAGuest', 'researcher']:
                         flairn=flair(submission.author.name)
                         print('  Flair: \'OC: {0}\' ({1})'.format(flairn, 'ocmaker'))
+                        r.subreddit(sub).flair.set(submission.author.name, 'OC: {0}'.format(flairn), 'ocmaker')
                     else:
                         print('  Flair: \'{0}\' ({1})'.format(submission.author_flair_text, submission.author_flair_css_class))
                     
@@ -158,15 +159,19 @@ while True:
         for submission in r.subreddit(sub).hot(limit=100):
             if (submission.author_flair_css_class not in ['w', 'practitioner', 'AMAGuest', 'researcher']) and (re.search('[\[\(][oO][cC][\]\)]', submission.title) is not None) and (submission.approved_by is not None):
                 flairn=flair(submission.author.name)
-                print('  Flaired {0} with \'OC: {1}\''.format(submission.author.name, flairn))
+                if flairn != int(submission.author_flair_text.split(' ')[1]):
+                    r.subreddit(sub).flair.set(submission.author.name, 'OC: {0}'.format(flairn), 'ocmaker')
+                    print('  Reflaired {0} with \'OC: {1}\''.format(submission.author.name, flairn))
+                else:
+                    print('  {0} has correct flair'.format(submission.author.name, flairn))
         print('Reflairing complete.\n')
         
     # Exception list for when Reddit inevitably screws up
     except praw.exceptions.APIException:
-        print('\n\nAn API exception happened.\nTaking a coffee break.\n')
+        print('\nAn API exception happened.\nTaking a coffee break.\n')
         time.sleep(30)
     except prawcore.exceptions.ServerError:
-        print('\n\nReddit\'s famous 503 error occurred.\nTaking a coffee break.\n')
+        print('\nReddit\'s famous 503 error occurred.\nTaking a coffee break.\n')
         time.sleep(180)
     except prawcore.exceptions.InvalidToken:
         print('\n401 error: Token needs refreshing.\nTaking a coffee break.\n')
@@ -174,5 +179,5 @@ while True:
     except (KeyboardInterrupt, SystemExit):
         raise
     except:
-        print('\n\nException happened.\nTaking a coffee break.\n')
+        print('\nException happened.\nTaking a coffee break.\n')
         time.sleep(30)
